d62249c66f4562a9cfd128789f7059413da928fe
Tkunkun <415268163@qq.com>
2021-10-04 14:01:54 +0800

    DIYICI

---
+++ src/main/java/com/tjk/tjk/TjkApplication.java
@@ -0,0 +1,13 @@
+package com.tjk.tjk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TjkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TjkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tjk/tjk/TjkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tjk.tjk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TjkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
